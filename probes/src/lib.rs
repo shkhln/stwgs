bb57538bf6af182a2b42a17ba4d1215f998a0a54
Alex S <shkhln@users.noreply.github.com>
2024-07-25 17:42:18 +0300

    Add SR2 probe configuration.

--- probes/src/lib.rs
+++ probes/src/lib.rs
@@ -9,7 +9,7 @@ custom_print::define_macros!({ eprint, eprintln, dbg }, concat, extern "C" fn pr
 custom_print::define_init_panic_hook!(concat, extern "C" fn print(_: *const u8, _: usize));
 
 extern "C" {
-  fn _register_probe(name: *const u8, executable: *const u8, flags: *const *const u8, flags_len: usize,
+  fn _register_probe(name: *const u8, executable: *const u8, flag_names: *const *const u8, flag_names_len: usize,
     init: extern "C" fn(u32, u32) -> i32 /* bool */, probe: extern "C" fn() -> u64);
 
   fn _add_screen_target(x1: f32, y1: f32, x2: f32, y2: f32) -> u32;
@@ -19,14 +19,14 @@ extern "C" {
   fn _peek_mem32(address: u32) -> u32;
 }
 
-fn register_probe(name: &str, executable: &str, flags: &[&str], test: extern "C" fn(u32, u32) -> i32, probe: extern "C" fn() -> u64) {
-  let name  = CString::new(name).unwrap();
-  let exe   = CString::new(executable).unwrap();
-  let flags = flags.iter()
+fn register_probe(name: &str, executable: &str, flag_names: &[&str], test: extern "C" fn(u32, u32) -> i32, probe: extern "C" fn() -> u64) {
+  let name       = CString::new(name).unwrap();
+  let exe        = CString::new(executable).unwrap();
+  let flag_names = flag_names.iter()
     .map(|flag_name| CString::new(*flag_name).unwrap().into_raw() as *const u8)
     .collect::<Vec<_>>();
   unsafe {
-    _register_probe(name.as_ptr() as *const u8, exe.as_ptr() as *const u8, flags.as_ptr(), flags.len(), test, probe);
+    _register_probe(name.as_ptr() as *const u8, exe.as_ptr() as *const u8, flag_names.as_ptr(), flag_names.len(), test, probe);
   }
 }
 
@@ -76,7 +76,7 @@ extern "C" fn gta_sa_probe() -> u64 {
 
   //TODO: different proportions
   let hud_is_visible = test_screen_target(0, 0.75, 1.0);
-  println!("hud_is_visible: {}", hud_is_visible);
+  //println!("hud_is_visible: {}", hud_is_visible);
 
   if hud_is_visible {
     let vehicle = peek_mem32(0x00C0FEE0);
@@ -110,7 +110,50 @@ extern "C" fn gta_sa_probe() -> u64 {
   }
 }
 
+static SR2_HELI_IDS:  &'static [u8] = &[11, 45, 84, 96, 103, 118];
+static SR2_PLANE_IDS: &'static [u8] = &[119, 97, 86, 24];
+
+extern "C" fn sr2_init(_screen_width: u32, _screen_height: u32) -> i32 {
+  true as i32
+}
+
+extern "C" fn sr2_probe() -> u64 {
+  let in_menu = peek_mem32(0x2348388) == 1; // 0x2347BCC, 0x2347BA8, 0x256DA24
+  if !in_menu {
+    let vehicle = peek_mem32(0x02CD2F60);
+    let flags   = peek_mem32(0x02CD3228); // some set of vehicle flags, presumably
+    if vehicle != 0 && flags & !0x10301 != 0 /* are we driving? */ {
+      assert_eq!(vehicle % 4, 0);
+
+      let whatever = peek_mem32(vehicle + 0x890); // unknown field in vehicle stats
+      assert_ne!(whatever, 0);
+      assert_eq!(whatever % 4, 0);
+
+      // we really shouldn't use negative offsets, something is not quite right there
+      let vehicle_id = peek_mem32(whatever - 0x530) as u8;
+      //println!("vehicle_id: {}", vehicle_id);
+
+      if SR2_HELI_IDS.iter().any(|id| *id == vehicle_id) {
+        return 1 << 3; // heli
+      }
+
+      if SR2_PLANE_IDS.iter().any(|id| *id == vehicle_id) {
+        return 1 << 4; // plane
+      }
+
+      1 << 2  // ride
+    } else {
+      1 << 1 // walk
+    }
+  } else {
+    1 << 0 // menu
+  }
+}
+
 #[no_mangle]
 pub extern "C" fn init() {
-  register_probe("GTA: SA", "gta-sa.exe", &["menu", "walk", "ride", "heli", "plane"], gta_sa_init, gta_sa_probe);
+  register_probe("Grand Theft Auto: San Andreas",
+    "gta-sa.exe", &["menu", "walk", "ride", "heli", "plane"], gta_sa_init, gta_sa_probe);
+  register_probe("Saints Row 2",
+    "SR2_pc.exe", &["menu", "walk", "ride", "heli", "plane"], sr2_init, sr2_probe);
 }
